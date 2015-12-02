604950f2e948e03938c0f99281d982e9d24d8a52
usuario <usuario@10.22.143.14>
2015-12-02 10:12:19 +0000

    Tercera Version

--- src/kata6/Kata6.java
+++ src/kata6/Kata6.java
@@ -2,16 +2,20 @@ package kata6;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Kata6 {
 
-       public static void main(String[] args) throws FileNotFoundException, IOException {
+       public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, SQLException {
             System.out.println("Inicio");
-            String name = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata6\\emails.txt";
+            String name = "emails.txt";
+            
             ArrayList<Person> listMail = MailList.read(name);
-            System.out.println(listMail.size());
-            HistogramBuilder <Person> builder = new HistogramBuilder<> (listMail);
+            ArrayList<Person1> person = ListLoader.read();
+            
+            HistogramBuilder<Person> builder = new HistogramBuilder<> (listMail);
+            HistogramBuilder<Person1> builder1 = new HistogramBuilder<> (person);
             
             Histogram<String> domains = builder.build(new Attribute<Person, String>() {
 
@@ -29,8 +33,39 @@ public class Kata6 {
                 }
             });
             
+            Histogram<String>  dominios = builder1.build(new Attribute<Person1, String>() {
+
+                @Override
+                public String get(Person1 item) {
+                    return item.getMail().split("@")[1];
+                }
+            });
+            
+            Histogram<Float> pesos = builder1.build(new Attribute<Person1, Float>() {
+
+                @Override
+                public Float get(Person1 item) {
+                    return item.getPeso();
+                }
+            });
+            
+            Histogram<Character> genero = builder1.build(new Attribute<Person1, Character>() {
+
+                @Override
+                public Character get(Person1 item) {
+                    return item.getGenero().charAt(0);
+                }
+            });
+            
+            
             new HistogramDisplay(domains, "DOMINIIOS").execute();
             new HistogramDisplay(letters, "CARACTER").execute();
+            new HistogramDisplay(dominios, "DOMINIOS").execute();
+            new HistogramDisplay(pesos, "PESOS").execute();
+            new HistogramDisplay(genero, "GENERO").execute();
+            
             System.out.println("Fin");
         }
+       
+      
 }
